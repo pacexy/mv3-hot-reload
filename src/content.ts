31924a5550c9281e822fecde5d9d22521001a196
Saiki K <pacexy@gmail.com>
2021-08-17 18:19:32 +0800

    fix: ws connection will be created in production mode

--- src/content.ts
+++ src/content.ts
@@ -1,8 +1,7 @@
 import { isDev, Message, PORT } from './utils'
 
-const ws = new WebSocket(`ws://localhost:${PORT}`)
-
 if (isDev) {
+  const ws = new WebSocket(`ws://localhost:${PORT}`)
   ws.addEventListener('message', (event) => {
     if (event.data === Message.FileChange) {
       chrome.runtime.sendMessage(Message.Reload)
