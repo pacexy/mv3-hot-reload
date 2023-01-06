9e0e751096a2657fbb6ab25de56dd139147cb6f0
idanrozin <idanrozin@gmail.com>
2023-01-06 19:27:24 +0200

    added option to support more dev environments

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,18 @@ export enum Message {
   Reload = 'reload',
 }
 
-export const isDev = process.env.NODE_ENV === 'development'
+const envs = ['development']
+
+const getEnvs = () => {
+  if (
+    process.env?.MV3_HOT_RELOAD_DEV_ENVS &&
+    Array.isArray(process.env?.MV3_HOT_RELOAD_DEV_ENVS)
+  ) {
+    envs.push(...process.env?.MV3_HOT_RELOAD_DEV_ENVS)
+  }
+  return envs
+}
+
+export const isDev = getEnvs().includes(process.env.NODE_ENV as string)
 
 export const PORT = process.env.MV3_HOT_RELOAD_PORT ?? 9012
