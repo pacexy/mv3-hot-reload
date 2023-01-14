6742042d9fe15d2dd75f7fb010d8625d2ab8ec1f
idanrozin <idanrozin@gmail.com>
2023-01-14 21:57:08 +0200

    removed optional chaining

--- src/utils.ts
+++ src/utils.ts
@@ -4,17 +4,15 @@ export enum Message {
 }
 
 const envs = ['development']
-
 const getEnvs = () => {
   if (
-    process.env?.MV3_HOT_RELOAD_DEV_ENVS &&
-    Array.isArray(process.env?.MV3_HOT_RELOAD_DEV_ENVS)
+    process.env.MV3_HOT_RELOAD_DEV_ENVS &&
+    Array.isArray(process.env.MV3_HOT_RELOAD_DEV_ENVS)
   ) {
-    envs.push(...process.env?.MV3_HOT_RELOAD_DEV_ENVS)
+    envs.push(...process.env.MV3_HOT_RELOAD_DEV_ENVS)
   }
   return envs
 }
 
 export const isDev = getEnvs().includes(process.env.NODE_ENV as string)
-
 export const PORT = process.env.MV3_HOT_RELOAD_PORT ?? 9012
